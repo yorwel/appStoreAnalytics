b7feb04d7bd076f3c1c1a5bdb435d09209ab82bb
Michael Wong <michael.wong@dxc.com>
2024-02-02 16:17:59 +0800

    push

--- main.py
+++ main.py
@@ -93,7 +93,7 @@ timestamp_string = current_time.isoformat()
 dt = datetime.strptime(timestamp_string, '%Y-%m-%dT%H:%M:%S.%f%z')
 date_time_str = dt.strftime('%d-%m-%Y %H:%M:%S')  # Date and time
 time_zone = dt.strftime('%z')  # Time zone
-output = f"Date and Time: {date_time_str}; Time zone: {time_zone}"
+output = f"{date_time_str}; GMT+{time_zone[2]} (SGT)"
 dateTime_df = pd.DataFrame(data = [output], columns = ['dateTime'])
 dateTime_df.to_csv(f"{folder_path}/dateTime.csv", header = True, index = False)
 dateTime_job_config = bigquery.LoadJobConfig(
