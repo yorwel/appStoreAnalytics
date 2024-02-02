73e8bf2b3aa0a92b91d60282a7f820cac79a7ef4
Michael Wong <michael.wong@dxc.com>
2024-02-02 23:47:53 +0800

    change name from android to google

--- main.py
+++ main.py
@@ -14,11 +14,11 @@ folder_path = os.getcwd().replace("\\", "/")
 project_id = "big-data-analytics-412816"
 dataset = "practice_project"
 apple_db_path = f"{project_id}.{dataset}.apple"
-android_db_path = f"{project_id}.{dataset}.android"
+google_db_path = f"{project_id}.{dataset}.google"
 
 client = bigquery.Client.from_service_account_json(f"{folder_path}/dataSources/big-data-analytics-412816-1be796546c90.json")
 apple_csv_path = f"{folder_path}/apple.csv"
-android_csv_path = f"{folder_path}/android.csv"
+google_csv_path = f"{folder_path}/google.csv"
 
 # Apple
 ## Clone the repository
@@ -30,7 +30,7 @@ subprocess.run(["tar", "-xvf", "appleAppData.json.tar.lzma"])
 ## Read into DataFrame
 apple = pd.read_json("appleAppData.json")
 
-# Android
+# Google
 ## Clone the repository
 subprocess.run(["git", "clone", "https://github.com/gauthamp10/Google-Playstore-Dataset.git"])
 ## Change directory to the dataset folder
@@ -45,22 +45,21 @@ with open(combined_csv, "wb") as outfile:
         with open(csvfile, "rb") as infile:
             outfile.write(infile.read())
 ## Read into DataFrame
-android = pd.read_csv("Google-Playstore-Dataset.csv", header = 0) # low_memory = False
+google = pd.read_csv("Google-Playstore-Dataset.csv", header = 0) # low_memory = False
 
 # Create tables into Google BigQuery
 ## Create 'apple' table in DB
 job = client.query(f"DELETE FROM {apple_db_path} WHERE TRUE").result()
 client.create_table(bigquery.Table(apple_db_path), exists_ok = True)
-## Create 'android' table in DB
-# job = client.query(f"DROP TABLE {android_db_path}").result()
-job = client.query(f"DELETE FROM {android_db_path} WHERE TRUE").result()
-client.create_table(bigquery.Table(android_db_path), exists_ok = True)
+## Create 'google' table in DB
+job = client.query(f"DELETE FROM {google_db_path} WHERE TRUE").result()
+client.create_table(bigquery.Table(google_db_path), exists_ok = True)
 
 # Save data as CSV files
 apple.columns = [name.replace(" ", "_") for name in apple.columns]
 apple.to_csv(apple_csv_path, header = True, index = False)
-android.columns = [name.replace(" ", "_") for name in android.columns]
-android.to_csv(android_csv_path, header = True, index = False)
+google.columns = [name.replace(" ", "_") for name in google.columns]
+google.to_csv(google_csv_path, header = True, index = False)
 
 # Push data into DB
 apple_job_config = bigquery.LoadJobConfig(
@@ -73,16 +72,16 @@ with open(apple_csv_path, 'rb') as f:
     apple_load_job = client.load_table_from_file(f, apple_config, job_config=apple_job_config)
 apple_load_job.result()
 
-android_job_config = bigquery.LoadJobConfig(
+google_job_config = bigquery.LoadJobConfig(
     autodetect=False,
     skip_leading_rows=1,
     max_bad_records=5,
     source_format=bigquery.SourceFormat.CSV
 )
-android_config = client.dataset(dataset).table('android')
-with open(android_csv_path, 'rb') as f:
-    android_load_job = client.load_table_from_file(f, android_config, job_config=android_job_config)
-android_load_job.result()
+google_config = client.dataset(dataset).table('google')
+with open(google_csv_path, 'rb') as f:
+    google_load_job = client.load_table_from_file(f, google_config, job_config=google_job_config)
+google_load_job.result()
 
 # Create 'dateTime' table in DB
 dateTime_csv_path = f"{folder_path}/dateTime.csv"
@@ -102,7 +101,6 @@ dateTime_job_config = bigquery.LoadJobConfig(
     source_format=bigquery.SourceFormat.CSV,
 )
 dateTime_config = client.dataset(dataset).table('dateTime')
-# job = client.query(f"DELETE FROM {project_id}.{dataset}.dateTime WHERE TRUE").result()
 with open(dateTime_csv_path, 'rb') as f:
     dateTime_load_job = client.load_table_from_file(f, dateTime_config, job_config=dateTime_job_config)
 dateTime_load_job.result()
@@ -110,7 +108,7 @@ dateTime_load_job.result()
 ## Remove CSV files and folder
 try:
     os.remove(apple_csv_path)
-    os.remove(android_csv_path)
+    os.remove(google_csv_path)
     os.remove(dateTime_csv_path)
     shutil.rmtree(f"{folder_path}apple-appstore-apps")
 except:
@@ -122,8 +120,8 @@ except:
 # """
 # apple_df = read_gbq(apple_query, project_id)
 
-# android_query = f"""
+# google_query = f"""
 #     SELECT *
-#     FROM {android_db_path}
+#     FROM {google_db_path}
 # """
-# android_df = read_gbq(android_query, project_id)
+# google_df = read_gbq(google_query, project_id)
